537bbe933a955aebcfb3d848c19f2ee335325ee7
shariernst <shariernst@mbp-von-shari.fritz.box>
2022-05-05 14:34:22 +0200

    try

---
+++ firstTry/src/gitTest/greeting.java
@@ -0,0 +1,11 @@
+package gitTest;
+
+public class greeting {
+	public static void main(String[] args) {
+		String a = "Greetings, welcome to the world!";
+		String b = "Hello";
+	
+		System.out.println(b+a);
+
+}
+}
